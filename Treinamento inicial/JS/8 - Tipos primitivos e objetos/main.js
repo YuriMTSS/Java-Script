0771c00747843508e2a7b383e8c4c83d5b7d64f6
Yuri Martins Silva <81636733+YuriMTSS@users.noreply.github.com>
2023-04-27 14:43:08 -0300

    Add files via upload

---
+++ Treinamento inicial/JS/8 - Tipos primitivos e objetos/main.js
@@ -0,0 +1,22 @@
+var str = 'Texto'
+var bl = true
+var und = undefined
+var nl = null
+var numb = 2.5
+var inte = 1
+
+
+
+var obj = {
+    idade: 25,
+    nome: 'Ana',
+    peso: 32.2,
+    altura: 1.50,
+    casado: false,
+}
+
+
+var fun = function (){
+    console.log(obj.nome);
+}
+fun();
