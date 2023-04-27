cc3d65a8c82557e1a831193784e8600d2a1b2d43
Yuri Martins Silva <81636733+YuriMTSS@users.noreply.github.com>
2023-04-27 14:37:08 -0300

    Add files via upload

---
+++ Treinamento inicial/JS/3 - Funcoes/main.js
@@ -0,0 +1,19 @@
+var x = 5;
+
+function func(x, y){
+    //x = 10;
+    //y = 10;
+    return x + y;
+    //return x + 10;
+    //return 100 * 50 / 3;
+}
+
+alert(func(10, 20));
+console.log(func(10, 20));
+
+var fun = function (){
+    console.log();
+}
+
+//alert(func());
+//console.log(func());
