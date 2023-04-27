7b5246b5af870078085dccb0073e70390fdbe4f5
Yuri Martins Silva <81636733+YuriMTSS@users.noreply.github.com>
2023-04-27 14:29:55 -0300

    Add files via upload

---
+++ Treinamento inicial/ts/3 - Objetos/type.ts
@@ -0,0 +1,15 @@
+let obj:{name: String, age: number, active?: boolean} ={
+    name: 'Yuri Martins Silva', age: 22,
+};
+
+let obj2:{[key: string]: any} = {
+    name: "Julio",
+    bar: true,
+    age: 25,
+};
+
+function fun(nome: string, sobrenome: string){
+
+}
+
+fun('nome', 'sobrenome');
