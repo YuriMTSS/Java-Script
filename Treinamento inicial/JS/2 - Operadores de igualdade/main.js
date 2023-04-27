5b9be23096c1eb39d0da5480f3626d2061b12710
Yuri Martins Silva <81636733+YuriMTSS@users.noreply.github.com>
2023-04-27 14:36:23 -0300

    Add files via upload

---
+++ Treinamento inicial/JS/2 - Operadores de igualdade/main.js
@@ -0,0 +1,8 @@
+2 == 2;     //Igual a
+3 != 2;     //Diferente de 
+5 === 5;    //Igual a, e do mesmo tipo
+2 !== 3;    //Diferente, mas do mesmo tipo
+5 > 2;      //Maior que
+2 < 5;      //Menor que
+5 >= 5;     //Maior ou igual a
+2 <= 4;     //Menos ou igual a
